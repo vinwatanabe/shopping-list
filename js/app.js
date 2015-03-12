8d667094b13d380471b945e77647ef5c79fe3540
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2015-03-11 23:52:11 -0300

    added some more js interaction to the page

--- js/app.js
+++ js/app.js
@@ -4,27 +4,44 @@ $(document).ready(function(){
 
 	/* defining check and delete button variables */
 	var actionButtons = '<div class = btn_delete></div>' + 
-						'<div class = btn_check></div>';
+						          '<div class = btn_check></div>';
 
-	/* gets the variable and adds it to the list when the add button is clicked */
+	/* gets the variable and adds it to the list when the add button is clicked, and hide the action buttons */
 	$('.btn-add').click(function(){
     	var newItem = $('.text-box').val();
-    	$('ul').append('<li>' + newItem + actionButtons + '</li>');
-    	$('.text-box').val('');
+
+      /* if user don't type */
+      if(newItem == ""){
+        alert('You need to type something.');
+      }else{
+        $('ul').append('<li>' + newItem + actionButtons + '</li>');
+        $('li').last().hide().slideDown('slow');
+         $('.text-box').val('');
+      }
   	});
+
 
   	/* when pressed enter */
   	$(document).keydown(function(event){
   		if(event.which===13){
   			var newItem = $('.text-box').val();
-    		$('ul').append('<li>' + newItem + actionButtons + '</li>');
-    		$('.text-box').val('');
+
+        /* if user don't type */
+      if(newItem == ""){
+        alert('You need to type something.');
+      }else{
+    		  $('ul').append('<li>' + newItem + actionButtons + '</li>');
+          $('li').last().hide().slideDown('slow');
+    		  $('.text-box').val('');
+        }
   		}
   	});
 
-  	/* delete the list item when the delete button is clickes */
+  	/* delete the list item when the delete button is clicked */
   	$('ul').on('click', '.btn_delete', function(){
-  		$(this).parent().remove();
+      $(this).parent().slideUp('slow', function(){
+        $(this).remove();
+      });
   	});
 
   	/* mark a item as checked */
